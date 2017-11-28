beddc564053140d200d97a12cbf266658f6dde81
aledts <aledts.gnr@gmail.com>
2017-11-28 17:53:28 -0200

    Projeto criado, gitignore configurado

---
+++ Tiro ao Alvaro/Assets/Scripts/TesteCubo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteCubo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Olar, to aqui");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Tiro ao Alvaro/Assets/Scripts/TesteOlar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteOlar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Olar mundo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
